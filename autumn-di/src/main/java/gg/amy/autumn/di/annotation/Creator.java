27a30513e59ded333e40810c30b67f5907bfb2de
amy null <null@amy.gg>
2021-05-01 16:04:35 -0400

    A few docs

--- autumn-di/src/main/java/gg/amy/autumn/di/annotation/Creator.java
+++ autumn-di/src/main/java/gg/amy/autumn/di/annotation/Creator.java
@@ -6,6 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <p>A creator method is a static method that takes only a {@link Class} as
+ * arguments, and returns a new instance of some value.</p>
+ * <p>An example use-case is a creator method that returns a new logger
+ * instance for the provided class.</p>
+ *
  * @author amy
  * @since 5/1/21.
  */
