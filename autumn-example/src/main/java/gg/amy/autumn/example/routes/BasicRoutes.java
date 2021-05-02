fd5c008d0147dcfbd8699be7c0ea7db43df0d6e4
amy null <null@amy.gg>
2021-05-02 15:23:56 -0400

    Fix body reading, POST example

--- autumn-example/src/main/java/gg/amy/autumn/example/routes/BasicRoutes.java
+++ autumn-example/src/main/java/gg/amy/autumn/example/routes/BasicRoutes.java
@@ -1,11 +1,13 @@
 package gg.amy.autumn.example.routes;
 
+import gg.amy.autumn.json.JsonObject;
 import gg.amy.autumn.web.annotation.Route;
 import gg.amy.autumn.web.http.HttpMethod;
 import gg.amy.autumn.web.http.Request;
 import gg.amy.autumn.web.http.Response;
 
 import javax.annotation.Nonnull;
+import java.util.Locale;
 
 /**
  * @author amy
@@ -20,5 +22,11 @@ public class BasicRoutes {
     @Route(method = HttpMethod.GET, path = "/henlo/:name")
     public Response henlo(@Nonnull final Request req) {
         return Response.create().body("henlo " + req.params().get("name") + '!');
+    }
+
+    @Route(method = HttpMethod.POST, path = "/upcase")
+    public Response json(@Nonnull final Request req) {
+        final var string = req.bodyString();
+        return Response.create().body(new JsonObject().put("upcased", string.toUpperCase(Locale.ROOT)).toJson());
     }
 }
--- autumn-web/src/main/java/gg/amy/autumn/web/netty/HttpChannelInboundHandler.java
+++ autumn-web/src/main/java/gg/amy/autumn/web/netty/HttpChannelInboundHandler.java
@@ -28,37 +28,45 @@ class HttpChannelInboundHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(@Nonnull final ChannelHandlerContext ctx, @Nonnull final Object msg) throws Exception {
         if(msg instanceof final FullHttpRequest request) {
-            final var start = System.nanoTime();
-            final var method = HttpMethod.fromNetty(request.method());
-            final var path = request.uri();
-            final var req = ImmutableRequest.builder()
-                    .id(ID.gen())
-                    .method(method)
-                    .path(path)
-                    .body(request.content().array())
-                    .params(Map.of())
-                    .build();
-            logger.info("{}: {} {}", req.id(), method.name(), path);
+            try {
+                final var start = System.nanoTime();
+                final var method = HttpMethod.fromNetty(request.method());
+                final var path = request.uri();
+                final var body = request.content();
+                final var bytes = new byte[body.readableBytes() ];
+                body.readBytes(bytes);
+                final var req = ImmutableRequest.builder()
+                        .id(ID.gen())
+                        .method(method)
+                        .path(path)
+                        .body(bytes)
+                        .params(Map.of())
+                        .build();
+                logger.info("{}: {} {}", req.id(), method.name(), path);
 
-            final var res = router.runRequest(req);
-            final var response = res.body();
-            final var status = res.status();
+                final var res = router.runRequest(req);
+                final var response = res.body();
+                final var status = res.status();
 
-            final HttpMessage out = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.valueOf(status), copiedBuffer(response));
-            if(HttpUtil.isKeepAlive(request)) {
-                out.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+                final HttpMessage out = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.valueOf(status), copiedBuffer(response));
+                if(HttpUtil.isKeepAlive(request)) {
+                    out.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+                }
+                // Helpful headers
+                if(!out.headers().contains(HttpHeaderNames.CONTENT_TYPE)) {
+                    out.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+                }
+                if(!out.headers().contains(HttpHeaderNames.CONTENT_LENGTH)) {
+                    out.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.length);
+                }
+
+                ctx.writeAndFlush(out);
+
+                logger.info("{}: status={} sentIn={}ms", req.id(), status, String.format("%.2f", (System.nanoTime() - start) / 1_000_000D));
+            } catch(final Throwable e) {
+                // TODO: Handling
+                e.printStackTrace();
             }
-            // Helpful headers
-            if(!out.headers().contains(HttpHeaderNames.CONTENT_TYPE)) {
-                out.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-            }
-            if(!out.headers().contains(HttpHeaderNames.CONTENT_LENGTH)) {
-                out.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.length);
-            }
-
-            ctx.writeAndFlush(out);
-
-            logger.info("{}: status={} sentIn={}ms", req.id(), status, String.format("%.2f", (System.nanoTime() - start) / 1_000_000D));
         } else {
             super.channelRead(ctx, msg);
         }
